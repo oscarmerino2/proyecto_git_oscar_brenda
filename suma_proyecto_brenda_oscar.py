9c6fc73b03fa9bebe9fd11797e7822bdd3818524
Brenda Estrada <bren.estrada94@gmail.com>
2022-11-23 13:17:18 +0100

    random seed y modificación de suma

--- suma_proyecto_brenda_oscar.py
+++ suma_proyecto_brenda_oscar.py
@@ -2,15 +2,20 @@
 # coding: utf-8
 
 # In[2]:
+import random
 
+random.seed(10)
 
-suma= (1+2)+3
+x = random.randint(1,10)
+y = random.randint(1,10)
+z = random.randint(1,10)
 
+suma = x+(y+z)
 
 # In[3]:
 
 
-suma
+print(suma)
 
 
 # In[ ]:
